b9a6e6bc47f3aa957936a974f4006e95ce31c373
Alexandre Arturi <alexandrea8@nycstudents.net>
2023-03-31 12:17:20 -0400

    3-31

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    public String toString(){
+        return "Point " + label + " is at x = " + x + ", y = " + y;
+    }
+
+    public boolean equals(Object obj){
+        if (!(obj instanceof Point)){
+            return false;
+        }
+        Point p = (Point)(obj);
+        return (this.x ==p.x) && (this.y==p.y);
+    }
 }
--- src/ThreeDPoint.java
+++ src/ThreeDPoint.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ThreeDPoint {
+public class ThreeDPoint extends Point{
+    private int z;
+
+    public ThreeDPoint(String label, int x, int y, int z){
+        super(label, x, y);
+        this.z = z;
+    }
+
+    public String toString(){
+        return super.toString() + ", z = " + z;
+    }
+
+    public boolean equals(Object obj){
+        if (!(obj instanceof ThreeDPoint)){
+            return false;
+        }
+
+        ThreeDPoint p = (ThreeDPoint) obj;
+        return super.equals(obj) && (this.z == p.z);
+    }
 }
